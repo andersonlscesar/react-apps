33dbf2f8fd8410ae6f237348724f097eaa2cb8f7
andersonlscesar <andersonlscesar@gmail.com>
2024-05-03 09:24:38 -0300

    form add new task pronto

--- 01-todolist/src/Components/CreateNewTask.js
+++ 01-todolist/src/Components/CreateNewTask.js
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 export default function CreateNewTask() {
+
+    const [ isUp, setIsUp ] = useState( false );
+
+    const bgIcon = { 
+        backgroundImage: `url("./img/${ !isUp ? 'plus.png' : 'minus.png' }")`
+    }
 
     return (
 
         <div className="add-new-task">
-            <button>Criar nova tarefa</button>
+            { isUp && <Form />}
+            <button style={ bgIcon } type="button" className="primary-button" onClick={ _ => setIsUp( up => !up ) }>Criar nova tarefa</button>
         </div>
 
     );
 
+}
+
+
+function Form() {
+
+    return (
+        <form className="add-new-task-content" >
+            <label>Título da nova tarefa</label>
+            <input type="text" id="new-task"/>
+            <button type="submit" className="secondary-button">Add</button>
+        </form>
+    );
 }
