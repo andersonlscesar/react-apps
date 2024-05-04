a1f62feb23f2015a7ea056ddb3e68e97019d9d2b
andersonlscesar <andersonlscesar@gmail.com>
2024-05-04 13:04:32 -0300

    form de nova tarefa pode ser cancelado

--- 01-todolist/src/Components/CreateNewTask.js
+++ 01-todolist/src/Components/CreateNewTask.js
@@ -9,8 +9,7 @@ export default function CreateNewTask() {
     return (
         <>
             { show && <FormTask onShow={ setShow }/> }
-            { !show && <button className="add-new-task-button" type="button" onClick={ _ => setShow( show => !show ) }></button> }
-    
+            { !show && <button className="add-new-task-button" type="button" onClick={ _ => setShow( show => !show ) }></button> }    
         </>
 
     );
--- 01-todolist/src/Components/FormTask.js
+++ 01-todolist/src/Components/FormTask.js
@@ -1,11 +1,11 @@
-export default function FormTask() {
+export default function FormTask( { onShow } ) {
 
     return (
 
         <form className="form-task">
             <div className="input-container">
                 <input type="text" placeholder="Descrição da tarefa..."/>
-                <button type="button" title="Cancelar" className="cancel-task"></button>
+                <button type="button" title="Cancelar" className="cancel-task" onClick={ _ => onShow( false ) }></button>
             </div>
             <button className="add-new-task-button submit" type="submit" title="enviar tarefa"></button>
         </form>
